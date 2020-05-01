546e672b2b3df5e18bd6f14485714bd6d35917b8
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2020-05-01 14:34:14 -0300

    Added Jest for testing

--- src/spell.spec.ts
+++ src/spell.spec.ts
@@ -1,5 +1,37 @@
 import Spell from './spell'
+// import jest from 'jest'
+// import { mocked } from 'ts-jest/utils'
 
-test('fulano', function () {
-  console.log('==> done')
+// interface Random {
+//   mockResolvedValue (number) {}
+// }
+
+// interface Math {
+//   random: Random
+// }
+
+describe('asd', function (){
+  test('Should cast a spell right away', function () {
+    const s = jest.spyOn(Math, 'random').mockReturnValue(0);
+    const spell = new Spell();
+    spell.cast()
+  })
+  test('Should cast a spell when magic words are correct', function () {
+    const s = jest.spyOn(Math, 'random').mockReturnValue(0);
+    const spell = new Spell();
+    const assert = () => {
+      spell.cast()
+    };
+    expect(assert).not.toThrow();
+  })
+
+  test('Should cast a spell when magic words are incorrect', function () {
+    const s = jest.spyOn(Math, 'random').mockReturnValue(1);
+    const spell = new Spell();
+    const assert = () => {
+      spell.cast()
+    };
+    expect(assert).toThrow();
+  })
 })
+
--- src/spell.ts
+++ src/spell.ts
@@ -2,12 +2,20 @@ export default class Spell {
     private MAGIC_WORD: string = 'Abracadabra'
     private GRIMOIRE: Array<string> = [ this.MAGIC_WORD ]
 
-    cast(useGrimoire = false):void {
-        if (useGrimoire || this.GRIMOIRE.includes(this.sayMagicWords())) {
-            console.log('Spell casted!')
-        } else {
-            throw new Error('You spell went really wrong!')
+    cast(useGrimoire?: boolean):void {
+        // console.log('useGrimoire', useGrimoire)
+        if (useGrimoire === undefined) {
+            if (this.sayMagicWords() === this.MAGIC_WORD) {
+                console.log('Spell casted!')
+            } else {
+                throw new Error('You spell went really wrong!')
+            }
         }
+        // if (useGrimoire) {
+        //     console.log('Spell casted!')
+        // } else {
+        //     throw new Error('You spell went really wrong!')
+        // }
     }
 
     private sayMagicWords():string{
