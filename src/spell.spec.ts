680da6d08ab43c3116d182e19116448ca4f31ffe
Rondinelli Mesquita <rondinellimesquitas@gmail.com>
2020-05-01 15:07:26 -0300

    Improve code

--- src/spell.spec.ts
+++ src/spell.spec.ts
@@ -1,37 +1,47 @@
 import Spell from './spell'
-// import jest from 'jest'
-// import { mocked } from 'ts-jest/utils'
 
-// interface Random {
-//   mockResolvedValue (number) {}
-// }
-
-// interface Math {
-//   random: Random
-// }
-
-describe('asd', function (){
+describe('Spell', function (){
   test('Should cast a spell right away', function () {
     const s = jest.spyOn(Math, 'random').mockReturnValue(0);
     const spell = new Spell();
     spell.cast()
   })
-  test('Should cast a spell when magic words are correct', function () {
-    const s = jest.spyOn(Math, 'random').mockReturnValue(0);
-    const spell = new Spell();
-    const assert = () => {
-      spell.cast()
-    };
-    expect(assert).not.toThrow();
+
+  describe('Should cast a spell', function () {
+    test('When magic words are correct', function () {
+      const s = jest.spyOn(Math, 'random').mockReturnValue(0);
+      const spell = new Spell();
+      const assert = () => {
+        spell.cast()
+      };
+      expect(assert).not.toThrow();
+    })
+
+    test('When using grimoire', function () {
+      const spell = new Spell();
+      const assert = () => {
+        spell.cast(true)
+      };
+      expect(assert).not.toThrow();
+    })
   })
 
-  test('Should cast a spell when magic words are incorrect', function () {
-    const s = jest.spyOn(Math, 'random').mockReturnValue(1);
-    const spell = new Spell();
-    const assert = () => {
-      spell.cast()
-    };
-    expect(assert).toThrow();
+  describe('Should not cast a spell', function () {
+    test('When magic words are incorrect', function () {
+      const s = jest.spyOn(Math, 'random').mockReturnValue(1);
+      const spell = new Spell();
+      const assert = () => {
+        spell.cast()
+      };
+      expect(assert).toThrow();
+    })
+    test('When not using a grimoire', function () {
+      const spell = new Spell();
+      const assert = () => {
+        spell.cast(false)
+      };
+      expect(assert).toThrow();
+    })
   })
 })
 
--- src/spell.ts
+++ src/spell.ts
@@ -3,24 +3,22 @@ export default class Spell {
     private GRIMOIRE: Array<string> = [ this.MAGIC_WORD ]
 
     cast(useGrimoire?: boolean):void {
-        // console.log('useGrimoire', useGrimoire)
-        if (useGrimoire === undefined) {
+        if (useGrimoire) {
+            console.log('Spell casted!')
+        } else if (useGrimoire == undefined){
             if (this.sayMagicWords() === this.MAGIC_WORD) {
                 console.log('Spell casted!')
             } else {
                 throw new Error('You spell went really wrong!')
             }
+        } else {
+            throw new Error('You spell went really wrong!')
         }
-        // if (useGrimoire) {
-        //     console.log('Spell casted!')
-        // } else {
-        //     throw new Error('You spell went really wrong!')
-        // }
     }
 
     private sayMagicWords():string{
         return Math.floor(Math.random() * 2) === 0
             ? this.MAGIC_WORD
-            : ''
+            : 'Hocus Pocus'
     }
 }
